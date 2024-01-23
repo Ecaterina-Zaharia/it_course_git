0abeb2ff6ad092f2f1c0817a57125c9f8adcec41
Home K&M <ecaterina.zah@gmail.com>
2024-01-23 16:30:51 +0200

    New code

---
+++ Calculater_UnitTests/src/Calculator.java
@@ -0,0 +1,56 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Calculator {
+
+    private int fistNumber;
+    private int secondNumber;
+
+    public void setFistNumber(int fistNumber) {
+        this.fistNumber = fistNumber;
+    }
+
+    public void setSecondNumber(int secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public int getFistNumber() {
+        return fistNumber;
+    }
+
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+
+    public Integer Addition(){
+        int sum = 0;
+        sum = this.fistNumber + this.secondNumber;
+        return sum;
+    }
+
+    public Integer Substraction(){
+        int subtr = 0;
+        subtr = this.fistNumber - this.secondNumber;
+        return subtr;
+    }
+
+    public Integer Multiplication(){
+        int mult = 0;
+        mult = this.fistNumber * this.secondNumber;
+        return mult;
+    }
+
+    public Integer Division(){
+        int div = 0;
+        try{
+            if(this.secondNumber == 0){
+                throw new ArithmeticException("Division be zero is not allowed");
+            } else {
+                div = this.fistNumber / this.secondNumber;
+
+            }
+        } catch (ArithmeticException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        return div;
+    }
+}
--- Calculater_UnitTests/src/Main.java
+++ Calculater_UnitTests/src/Main.java
@@ -5,37 +5,18 @@ public class Main {
 
         Scanner scanner1 = new Scanner(System.in);
 
+        Calculator calculator = new Calculator();
+
+        UserInterface userInterface = new UserInterface(scanner1, calculator);
+
         System.out.println("Enter the first Integer: ");
         int i1 = scanner1.nextInt();
+        calculator.setFistNumber(i1);
 
         System.out.println("Enter the second Integer: ");
         int i2 = scanner1.nextInt();
+        calculator.setSecondNumber(i2);
 
-        System.out.println("Input your option: \n" +
-                "1-Addition.\n" +
-                "2-Substraction.\n" +
-                "3-Multiplication.\n" +
-                "4-Division.\n" +
-                "5-Exit.");
-        int option = scanner1.nextInt();
-
-        if (option == 1){
-            System.out.println("The Addition of " + i1 + " and " + i2 + " is: " + (i1 + i2));
-        }
-        else if (option == 2){
-            System.out.println("The Substraction of " + i1 + " and " + i2 + " is: " + (i1 - i2));
-        }
-        else if (option == 3){
-            System.out.println("The Multiplication of " + i1 + " and " + i2 + " is: " + (i1 * i2));
-        }
-        else if (option == 4){
-            System.out.println("The Division of " + i1 + " and " + i2 + " is: " + (i1 / i2));
-        }
-        else if (option == 5) {
-            System.out.println("Exit");
-        }
-        else {
-            System.out.println("The wrong input. Please, try again");
-        }
+        userInterface.start();
     }
 }
